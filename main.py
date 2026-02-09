741b51fc1d963f8904a86c5cf959a10194a8e915
fengziyinfeng <131565538+fengziyinfeng@users.noreply.github.com>
2026-02-09 23:03:09 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def resource_path(relative_path):
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
+    return os.path.join(base_path,relative_path)
 
 #获取游戏DPI
 ctypes.windll.user32.SetProcessDPIAware()
@@ -35,14 +35,20 @@ EXCLAMATION_TEMPLATE = cv2.imread(
 if EXCLAMATION_TEMPLATE is None:
     raise FileNotFoundError("❌ 找不到 exclamation_mark.png")
 
-#加载清理背包按钮模板
-button_templates = {
-    "AllSell": resource_path("AllSell.png"),
-    "All": resource_path("All.png"),
-    "OK1": resource_path("OK1.png"),
-    "OK2": resource_path("OK2.png"),
-    "Back": resource_path("Back.png"),
-    "shaonv": resource_path("shaonv.png"),
+#加载UI模板
+UI_TEMPLATES = {
+    "AllSell": resource_path("TEMPLATES/AllSell.png"),
+    "All": resource_path("TEMPLATES/All.png"),
+    "OK1": resource_path("TEMPLATES/OK1.png"),
+    "OK2": resource_path("TEMPLATES/OK2.png"),
+    "Back": resource_path("TEMPLATES/Back.png"),
+    "shaonv": resource_path("TEMPLATES/shaonv.png"),
+    "change": resource_path("TEMPLATES/change.png"),
+    "map1": resource_path("TEMPLATES/map1.png"),
+    "map2": resource_path("TEMPLATES/map2.png"),
+    "map3": resource_path("TEMPLATES/map3.png"),
+    "map4": resource_path("TEMPLATES/map4.png"),
+    "Go": resource_path("TEMPLATES/Go.png"),
 }
 
 # ================== 获取窗口区域信息 ==================
@@ -66,7 +72,28 @@ def get_window_screenshot(region):
 region = get_window_region(GAME_TITLE)
 if region is None:
     exit(1)
-
+    
+#获取游戏窗口中心坐标
+def get_window_center():
+    if region:
+        # 计算窗口的中心坐标
+        center_x = region['left'] + region['width'] // 2
+        center_y = region['top'] + region['height'] // 2
+        return {
+        "name": "window_center",
+        "x": center_x,
+        "y": center_y,
+        "w": center_x,
+        "h": center_y,
+        "score": 1
+    }
+    else:
+        return None
+    
+#设置窗口中心
+region_center = get_window_center()
+if region_center is None:
+    exit(1)
 # ================== 基础设置 ==================
 
 # 定义感叹号检测区域
@@ -107,6 +134,12 @@ BUTTON_REGIONS = {
     "OK2":     (0.3, 0.3, 0.8,0.8),
     "Back":    (0,0, 0.3, 0.3),
     "shaonv":  (0,0, 0.3, 0.3),
+    "change":  (0,0, 0.3, 0.3),
+    "map1":  (0,0, 1, 1),
+    "map2":  (0,0, 1, 1),
+    "map3":  (0,0, 1, 1),
+    "map4":  (0,0, 1, 1),
+    "Go":  (0.75, 0.5, 1, 1),
 }
 
 #计算区域坐标
@@ -118,6 +151,61 @@ def calc_roi(region, ratio):
         "width": int(region["width"] * (r - l)),
         "height": int(region["height"] * (b - t)),
     }
+#===================通用方法=========================
+#查找UI位置
+def match_UI(UI_name, region, threshold=0.5):
+    template_raw = cv2.imread(
+        UI_TEMPLATES[UI_name],
+        cv2.IMREAD_GRAYSCALE
+    )
+    
+    if template_raw is None:
+        print(f"❌ 模板缺失: {UI_name}")
+        return None
+
+    scale = region["width"] / 1920
+    tw = int(template_raw.shape[1] * scale)
+    th = int(template_raw.shape[0] * scale)
+
+    if tw < 5 or th < 5:
+        return None
+    template = cv2.resize(template_raw, (tw, th), cv2.INTER_AREA)
+    roi = calc_roi(region, BUTTON_REGIONS[UI_name])
+    screenshot = get_window_screenshot(roi)
+    gray = cv2.cvtColor(screenshot, cv2.COLOR_BGRA2GRAY)
+
+    res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
+    _, max_val, _, max_loc = cv2.minMaxLoc(res)
+
+    print(f"🔎 {UI_name} 匹配度: {max_val:.3f}")
+
+    if max_val < threshold:
+        return None
+
+    x = roi["left"] + max_loc[0]
+    y = roi["top"] + max_loc[1]
+
+    return {
+        "name": UI_name,
+        "x": x,
+        "y": y,
+        "w": tw,
+        "h": th,
+        "score": max_val
+    }
+
+#计算中心点
+def center_of(box):
+    cx = box["x"] + box["w"] // 2
+    cy = box["y"] + box["h"] // 2
+    return cx, cy
+
+#点击
+def click_UI(cx, cy):
+    pydirectinput.moveTo(cx, cy)
+    time.sleep(0.25)
+    pydirectinput.click()
+    print(f"🖱 点击 ({cx}, {cy}) ")
 
 # ================== 钓鱼 ==============================
 
@@ -168,9 +256,9 @@ def match_exclamation_multi_scale(gray, template):
 
 #感叹号检测
 def wait_for_bite(sct,threshold):
-    global fail_num
     print("⏳ 等待上钩...")
     start_time = time.time()
+    global fail_num
     
     # 短暂延迟，避开抛竿动画
     time.sleep(0.5)
@@ -336,75 +424,154 @@ def handle_timeout():
     
 #===========================背包清理=============================
 
-#查找按钮位置
-def find_button_location(button_name, region):
-    template_raw = cv2.imread(button_templates[button_name], cv2.IMREAD_GRAYSCALE)
-    if template_raw is None:
-        print(f"❌ 模板缺失: {button_name}")
-        return None
-
-    scale = region["width"] / 1920
-    tw = int(template_raw.shape[1] * scale)
-    th = int(template_raw.shape[0] * scale)
-    if tw < 5 or th < 5:
-        return None
-
-    template = cv2.resize(template_raw, (tw, th), cv2.INTER_AREA)
-    roi = calc_roi(region, BUTTON_REGIONS[button_name])
-    screenshot = get_window_screenshot(roi)
-    gray = cv2.cvtColor(screenshot, cv2.COLOR_BGRA2GRAY)
-
-    res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
-    _, max_val, _, max_loc = cv2.minMaxLoc(res)
-
-    print(f"🔎 {button_name} 匹配度: {max_val:.3f}")
-
-    if max_val < 0.5:
-        return None
-
-    x = roi["left"] + max_loc[0]
-    y = roi["top"] + max_loc[1]
-    return (x, y), (tw, th)
-
-#点击
-def click_button(location):
-    (x, y), (w, h) = location
-    cx = x + w // 2
-    cy = y + h // 2
-    pydirectinput.moveTo(cx, cy)
-    time.sleep(0.1)
-    pydirectinput.click()
-    print(f"🖱️ 点击坐标: ({cx}, {cy})  [按钮区域: x={x}, y={y}, w={w}, h={h}]")
-    time.sleep(0.5)
-
 #背包清理
 def clean_backpack():
+    time.sleep(1)
     print("🧹 清理背包")
-    print("📂 尝试打开背包，每十秒自动重按下 T ")
-    while True:
+    print("📂 尝试打开背包：每 5 秒按一次 T，最多 3 次")
+
+    for attempt in range(1, 4):
+        print(f"🔁 第 {attempt} 次尝试打开背包")
         pydirectinput.press("t")
 
-        start_time = time.time()
-        entered = False
-
-        while time.time() - start_time < 10:
-            loc = find_button_location("shaonv", region)
-            if loc:
+        start = time.time()
+        while time.time() - start < 5:
+            if match_UI("shaonv", region):
                 print("✅ 已成功进入背包界面")
-                entered = True
                 break
-            time.sleep(1)
+            time.sleep(0.5)
+        else:
+            continue
 
-        if entered:
-            break  # 成功进入背包，跳出外层 while
+        for name in ["AllSell", "All", "OK1", "OK2", "Back"]:
+            box = match_UI(name, region)
+            cx, cy = center_of(box)
+            if box:
+                print(f"正在处理{name}，匹配度：{box['score']:.3f}")
+                click_UI(cx, cy)
+                time.sleep(1)
 
+        print("🔄 背包清理完成")
+        return
+
+    print("❌ 多次尝试仍未进入背包，跳过本次清理")
+
+#===========================地图切换=============================
+#点击更换按钮
+def click_change():
+    # 检查 change 按钮并点击
+    def click_if_found(button_name):
+        region = get_window_region(GAME_TITLE)
+        box = match_UI(button_name, region)
+        if box:
+            cx, cy = center_of(box)
+            click_UI(cx, cy)  # 点击按钮
+            print(f"✅ 按钮 '{button_name}' 被点击")
+            return True
+        return False
+
+    # 初次检测并点击 'change' 按钮
+    while True:
+        if click_if_found("change"):
+            print("⏳ 等待10秒")
+            time.sleep(10)  # 等待10秒以确保不是在切换白天黑夜
+            break
+        else:
+            time.sleep(0.5)
     
-    for name in ["AllSell", "All", "OK1", "OK2", "Back"]:
-        loc = find_button_location(name, region)
-        if loc:
-            click_button(loc)
-            time.sleep(1)
+    # 第二次检测并点击
+    if click_if_found("change"):
+        print("⏳ 等待5秒")
+        time.sleep(5)  # 等待5秒
 
+    # 第三次检测，如果按钮依然存在，认为点击失败
+    if match_UI("change", get_window_region(GAME_TITLE)):
+        print("❌ 仍检测到 'change' 按钮，切换失败")
+        return False
+    else:
+        print("✅ 没有检测到 'change' 按钮，已成功切换")
+        return True
+    
+#对地图按钮进行匹配，返回匹配值最低的一个
+def get_lowest_match_map(region):
+    min_match = float("inf")
+    lowest_map = None
+    
+    for map_name in ["map1", "map2", "map3", "map4"]:
+        box = match_UI(map_name, region)
+        name, match_val = box["name"],box["score"]
+        if match_val < min_match:
+            min_match = match_val
+            lowest_map = name
+    
+    print(f"最低匹配度的图标是: {lowest_map}, 匹配度: {min_match:.3f}")
+    print(f"判定当前所在地图为{lowest_map}")
+    return lowest_map, min_match
+
+#切换地图
+def change_map(map_index):
+    map_name = f"map{map_index}"
+    time.sleep(1)
+    next_map = "map1"
+
+    #点击更换按钮
+    if not click_change():
+        return
+    region = get_window_region(GAME_TITLE)
+    lowest_map, min_match = get_lowest_match_map(region)
+    
+    #匹配度最低的判定为当前所在地图
+    if lowest_map == "map1":
+        #当前所在地图为1就去2
+        print("切换至其他地图")
+        for  name in ["map2", "Go", "OK2"]:
+            region = get_window_region(GAME_TITLE)
+            box = match_UI(name, region)
+            cx, cy = center_of(box)
+            if box:
+                click_UI(cx, cy)
+                time.sleep(1)
+        print("⏳ 等待切换地图15秒")
+        time.sleep(15)
+        #切换至图1
+        print("切换至预设地图")
+        if not click_change():
+            return
+        for  name in ["map1", "Go", "OK2"]:
+            region = get_window_region(GAME_TITLE)
+            box = match_UI(name, region)
+            cx, cy = center_of(box)
+            if box:
+                click_UI(cx, cy)
+                time.sleep(1)
+        print("⏳ 等待切换地图15秒")
+        time.sleep(15)
+    else:
+        #当前所在地图不是1就去1
+        print("切换至其他地图")
+        for  name in ["map1", "Go", "OK2"]:
+            region = get_window_region(GAME_TITLE)
+            box = match_UI(name, region)
+            cx, cy = center_of(box)
+            if box:
+                click_UI(cx, cy)
+                time.sleep(1)
+        print("⏳ 等待切换地图15秒")
+        time.sleep(15)
+        #切换至想去的地图
+        if not click_change():
+            return
+        print("切换至预设地图")
+        for  name in [map_name, "Go", "OK2"]:
+            region = get_window_region(GAME_TITLE)
+            box = match_UI(name, region)
+            cx, cy = center_of(box)
+            if box:
+                click_UI(cx, cy)
+                time.sleep(1)
+        print("⏳ 等待切换地图15秒")
+        time.sleep(15)
+     
 #############################################################################################
 def main():
     print("！！！注意！！！！！！")
@@ -412,17 +579,33 @@ def main():
     print("建议设置分辨率FHD。HD未测试，不确定能不能行")
     
     threshold_input = input("请输入感叹号匹配度阈值（默认值 0.85，直接按回车使用默认值）: ")
-    threshold = float(threshold_input) if threshold_input else 0.85  # 默认为0.85
-    
+    try:
+        threshold = float(threshold_input) if threshold_input else 0.85
+    except ValueError:
+        print("⚠️ 输入非法，已自动使用默认阈值 0.85")
+        threshold = 0.85
     print(f"感叹号检测阈值已设置为: {threshold}")
-    print("如果在抛竿时直接进入qte阶段，并且持续抛竿，说明阈值过低")
+    print("如果在抛竿后直接进入qte阶段，并且持续抛竿，说明阈值过低")
+    
+    map_input = input("请输入每5小时30分钟自动切换的地图序号（默认值 1，直接按回车使用默认值）: ")
+    print("如果开始就在图1，则会切换到图2，紧接着切换回来")
+    try:
+        map_index = int(map_input) if map_input else 1
+    except ValueError:
+        print("⚠️ 输入非法，已自动使用默认地图 1")
+        map_index = 1
+    print(f"🗺 当前选择地图: {map_index}")
+    
+
     print("请3秒内手动切换至游戏窗口，脚本将会启动")
+
+    start_time = time.time()
     time.sleep(3)
     global fail_num
     
     print("\n🚀 启动自动钓鱼\n")
     time.sleep(0.5)
-    
+
     with mss.mss() as sct:
         cycle = 0
         while True:
@@ -430,13 +613,22 @@ def main():
             print("====================================================================")
             print(f"🔁 第 {cycle} 轮钓鱼开始")
 
+            #累积运行5小时30分钟（19800秒）会切换地图
+            elapsed_time = time.time() - start_time
+            if elapsed_time >= 19800:
+                change_map(map_index)
+                start_time = time.time()
+                
+            #累计三次识别感叹号失败会清理背包
             if fail_num >= 3:
                 clean_backpack()
                 fail_num = 0
                 continue
-
+            
+            #钓鱼操作
             cast_rod()
             if wait_for_bite(sct,threshold):
+                fail_num = 0
                 play_qte(sct)
             finish_fishing(region["left"] + region["width"]//2, region["top"] + region["height"]//2)
             time.sleep(1.5)
